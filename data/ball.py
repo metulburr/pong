6d1197dff7f886e3776234d2c08cdcd7527277e5
metulburr <micahpage911@yahoo.com>
2013-12-17 17:02:49 -0500

    added ball speed increase

--- data/ball.py
+++ data/ball.py
@@ -11,7 +11,10 @@ class Ball:
         self.center_screen = screen_rect.center
         self.color = color
         self.surface.fill(self.color)
-        self.speed = 5
+        self.speed_init = 3
+        self.speed = self.speed_init
+        self.speed_incr = 0
+        self.switch_speed = 5
         self.set_ball()
         self.sound_init()
         
@@ -25,6 +28,9 @@ class Ball:
         self.vel = [random.choice([-1, 1]), random.choice([-1, 1])]
         self.rect.center = self.center_screen
         self.true_pos = list(self.rect.center)
+        
+        self.speed = self.speed_init #reset speed
+        self.speed_incr = 0
         
     def collide_walls(self):
         if self.rect.x < 0:
@@ -44,6 +50,7 @@ class Ball:
         if self.rect.colliderect(paddle_left_rect) or self.rect.colliderect(paddle_right_rect):
             self.vel[0] *= -1;
             self.bounce.sound.play()
+            self.speed_incr += 1
             
     def move(self):
         self.true_pos[0] += self.vel[0] * self.speed
@@ -56,6 +63,9 @@ class Ball:
             return hit_side
         self.move()
         self.collide_paddle(paddle_left_rect, paddle_right_rect)
+        if self.speed_incr >= self.switch_speed:
+            self.speed += 1
+            self.speed_incr = 0
 
     def render(self, screen):
         screen.blit(self.surface, self.rect)
