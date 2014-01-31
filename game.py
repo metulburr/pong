dce1d1a548a67a7d0d941f7eb3705be33e779c46
metulburr <micahpage911@yahoo.com>
2014-01-31 02:56:59 -0500

    modified arg parser

--- game.py
+++ game.py
@@ -6,15 +6,15 @@ import data.tools
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='')
+parser = argparse.ArgumentParser(description='Pong Arguments')
 parser.add_argument('-c','--clean', action='store_true', 
     help='Remove all .pyc files and __pycache__ directories')
 parser.add_argument('-f' , '--fullscreen', action='store_true',
     help='start program with fullscreen')
 parser.add_argument('-d' , '--difficulty', default='medium',
     help='where DIFFICULTY is one of the strings [hard, medium, easy], set AI difficulty, default is medium, ')
-parser.add_argument('-s' , '--size', nargs=2, default=[800,600],
-    help='where SIZE SIZE is WIDTH HEIGHT, set window size, defualt is 800 600')
+parser.add_argument('-s' , '--size', nargs=2, default=[800,600], metavar=('WIDTH', 'HEIGHT'),
+    help='set window size to WIDTH HEIGHT, defualt is 800 600')
 args = vars(parser.parse_args())
 
 if __name__ == '__main__':
